38bae830929a37ecefff6b1585eb54ec6d5a8855
Jamie <jamo.marshall+github@gmail.com>
2022-02-15 11:55:09 +0800

    finished CVE API interaction

--- MK_0_Scraper.sh
+++ MK_0_Scraper.sh
@@ -2,10 +2,12 @@
 
 ###202202141411 --> Website dump completed
 ###202202151131 --> Point 2 completed
+###202202151154 --> Finished CVE API interaction
+
 
 ### 1. scrape the relevent website --> tick
 ### 2. clean the output leaving only the raw CVE numbers
-###
+### 3. for loop requesting CVE from NVD API
 
 
 website_dump(){
@@ -17,10 +19,14 @@ raw_CVEs(){
     cleaned=$(grep -P ">CVE-[[:digit:]]{4}-[0-9]*<" "output.txt" | sed "s/.*\" >//g" | sed "s/<.*//g")
 }
 
+request_CVE_NVD_API(){
+    raw_CVEs
 
-raw_CVEs
+    for n in $cleaned;do
+    curl "https://services.nvd.nist.gov/rest/json/cve/1.0/$n" > "$n.txt"
+    sleep 1s
+    done
 
-for n in $cleaned;do
-    echo $n
-    sleep 5s
-done
+}
+
+request_CVE_NVD_API
