50fe99a7e982ecca001aec0db95648a9d5b92d5e
Jamie <jamo.marshall+github@gmail.com>
2022-02-16 09:57:58 +0800

    modified awk format

--- MK_0_Scraper.sh
+++ MK_0_Scraper.sh
@@ -5,16 +5,18 @@
 ###202202151154 --> Finished CVE API interaction
 ###202202151639 --> Parsing more data fields from the JSON CVE files
 ###202202160735 --> Fixed bug in file loop function
+###202202160735 --> Finished the variable scraping from JSON and formatting for awk
+###202202160735 --> Modified the awk format file to remove newlines
 
 ### 1. scrape the relevent website --> tick
 ### 2. clean the output leaving only the raw CVE numbers
 ### 3. for loop requesting CVE from NVD API
 ### 4. Scrape a summary of information for each CVE
 ### 5. awk for each of the above summarys -|-> which will be incorpreated with (tester_looping_files) and find_CVE_Data
-### 6. Add date function to make new directory per CVE dump.
+### 6. Add date function to make new directory per CVE dump......maybe
 
 ### current process flow 
-### (tester_looping_files+(find_CVE_Data)TBA) -|-> request_CVE_NVD_API --> raw_CVEs --> website_dump
+### tester_looping_files -calls $1-> find_CVE_Data -calls $1-> pretty_awk_fix --> request_CVE_NVD_API --> raw_CVEs --> website_dump
 
 website_dump(){
     curl "https://nvd.nist.gov/" > "output.txt" 
@@ -49,18 +51,45 @@ find_CVE_Data(){
 
     #this code will need to loop 20 times, creating an awk formated document for each variable
 
-   refrences=$(grep "\"url\": \"" "NewCve/$1" | awk '{$1=$1};1' | sed 's/\"url\": \"/:/g' | sed 's/\",//g')
+   refrences=$(grep "\"url\": \"" "NewCve/$1" | awk '{$1=$1};1' | sed 's/.*:/:/g' | sed 's/\",//g')
    CVE_ID=$(cat "NewCve/$1" | jq .result.CVE_Items | jq .[0].cve.CVE_data_meta.ID)
    CWE_ID=$(cat "NewCve/$1" | jq .result.CVE_Items | jq .[0].cve.problemtype.problemtype_data | jq .[0].description | jq .[0].value)
    severity=$(cat "NewCve/$1" | jq .result.CVE_Items | jq .[0].impact.baseMetricV3.cvssV3.baseSeverity)
    
 
-   formatted_for_awk=$(echo -e "$CVE_ID:$CWE_ID:$severity$refrences")
-   pretty_awk_fix "$formatted_for_awk"
+   echo -e "$CVE_ID:$CWE_ID:$severity$refrences" | tr -d '\n' > "format/$CVE_ID-$severity"
+   file_format_awk=$CVE_ID-$severity
+   pretty_awk_fix "$file_format_awk"
 }
 
 pretty_awk_fix(){
-echo $1
+awk 'BEGIN { 
+      
+    FS=":"; 
+      
+    print "_____________________________________________"; 
+      
+    print "|\033[34mCVE ID\033[0m               |\033[34mCWE ID\033[0m            | \033[34mSeverity\033[0m                                                    |"; 
+      
+} 
+      
+{ 
+      
+    printf("| %-19s | %-16s | %-60s|\n", $1, $2, $3); 
+    printf("| %-100s |\n", $4);
+    printf("| %-100s |\n", $5);
+    printf("| %-100s |\n", $6);
+    printf("| %-100s |\n", $7);
+      
+} 
+      
+END { 
+      
+    print("_____________________________________"); 
+      
+}' "format/$1"
+
 }
 
 tester_looping_files
+              
