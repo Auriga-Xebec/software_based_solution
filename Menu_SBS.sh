1d9443750c858255145bfe9658e2ee4116758b19
Auriga-Xebec <97719033+Auriga-Xebec@users.noreply.github.com>
2022-02-18 11:26:50 +0800

    Started menu for SBS

---
+++ Menu_SBS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### this will be the menu for the software based soluton
+
+while true; do
+echo -e "1) Scrape Aus Cyber for latest threats
+2) Scrape NVD for the 20 latest scored CVEs
+3) Search for a CVE summary"
+read selection
+echo $selection
+
+
+done
