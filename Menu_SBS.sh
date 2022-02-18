eecdd3f07361b6c0eecba3710f290429e1c878aa
Auriga-Xebec <97719033+Auriga-Xebec@users.noreply.github.com>
2022-02-18 11:39:08 +0800

    fixed the case statement

--- Menu_SBS.sh
+++ Menu_SBS.sh
@@ -5,9 +5,28 @@
 while true; do
 echo -e "1) Scrape Aus Cyber for latest threats
 2) Scrape NVD for the 20 latest scored CVEs
-3) Search for a CVE summary"
+3) Search for a CVE summary
+4) Exit"
+
 read selection
-echo $selection
 
+case "$selection" in
 
+    1 )
+        echo "you have selected option 1"
+    ;;
+    2 )
+        echo "you have selected option 2"
+    ;;
+    3 )
+        echo "you have selected option 3"
+    ;;
+    4 )
+        echo "you have selected option 4"
+        break 
+    ;;
+    * )
+        echo "Failed to select 1-4"
+    ;;
+esac
 done
