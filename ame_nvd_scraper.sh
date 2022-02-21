cda0ee030acdc1ea13d8e69422b66cdbdfc802cc
Auriga-Xebec <97719033+Auriga-Xebec@users.noreply.github.com>
2022-02-21 11:49:07 +0800

    Finished GAWK formatting of JSON

--- ame_nvd_scraper.sh
+++ ame_nvd_scraper.sh
@@ -89,7 +89,7 @@ JSON_cve_information_dump(){
 }
 #JSON_cve_information_dump
 
-
+JSON_Parse(){
 CVE_ID=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].cve.CVE_data_meta.ID | sed 's/"//g')
 CVE_Assigner=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].cve.CVE_data_meta.ASSIGNER | sed 's/"//g')
 CVE_description=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].cve.description.description_data | jq .[0].value | sed 's/"//g')
@@ -111,4 +111,21 @@ baseSeverity=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].
 publishedDate=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].publishedDate | sed 's/[A-Z]/ /g' | sed 's/"//g')
 lastModifiedDate=$(cat "test_extrap_more_info.txt" | jq .result.CVE_Items | jq .[0].lastModifiedDate | sed 's/[A-Z]/ /g' | sed 's/"//g') 
 
+echo "$CVE_ID-->$CVE_Assigner-->$CVE_CWE-->$impact_vectorString-->$impact_attackVector-->$impact_attackComplexity-->$impact_privilegesRequired-->$impact_userInteraction-->$impact_confidentialityImpact-->$impact_integrityImpact-->$impact_availabilityImpact-->$baseSeverity-->$publishedDate-->$lastModifiedDate-->$CVE_description" > JSON_parsed.txt
+
+#-->$CVE_description-->$CVE_refs
+}
+JSON_Parse
+
+gawk 'BEGIN {FS = "-->" ; OFS = "\t"} {print $1,$2,$3,$12,$4"\n"}
+{print "Description:\n"$15"\n"}
+{print "Attack vector = " $5}
+{print "Attack complexity = " $6}
+{print "Privledges required = " $7}
+{print "User interaction = " $8 "\n"}
+{print "Confidentiality Impact = " $9}
+{print "Integrity Impact = " $10}
+{print "Availability Impact = " $11 "\n"}
+{print "Published = " $13} 
+{print "Last Modified = " $14}' JSON_parsed.txt
 
