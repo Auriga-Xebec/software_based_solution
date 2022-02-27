e9d6455b0fc20b734185d7d048e5350171450b1d
Auriga-Xebec <97719033+Auriga-Xebec@users.noreply.github.com>
2022-02-27 11:37:21 +0800

    elaboration of design

--- aus_cyber_scrape.sh
+++ aus_cyber_scrape.sh
@@ -3,6 +3,8 @@
 ### can I save this to a variable then call grep on it, to save the unessasary use of files, or maybe use a tempoary file.
 raw_html=$(curl "https://www.cyber.gov.au/acsc/view-all-content/alerts")
 
+
+### TAkes the raw html from above and scrapes out the date, severity, summary, elaboration and URL
 scrape_HTML(){
 alert_date=($(echo "$raw_html" | grep  "<p class=\"acsc-date\">" | sed 's/.*e\">//' | sed 's/ -.*//' | sed 's/ /-/g' )) ### date of the alert.
 alert_severity=($(echo "$raw_html" | grep  "<p class=\"acsc-date\">" | sed 's/.*alert-//' | sed 's/">.*//')) ### severity of the alert.
@@ -14,6 +16,7 @@ alert_summary=($(echo "$raw_html" | grep   "<p class=\"acsc-summary\">" | sed 's
 alert_elaboration=($(echo "$raw_html" | grep   "<p class=\"acsc-title\">" | sed 's/.*e">//g' | sed 's/<\/p>//g'))
 alert_elaboration_url=($(echo "$raw_html" | grep   "<p class=\"acsc-title\">" | sed 's/.*e">//g' | sed 's/<\/p>//g' | sed 's/ /-/g'))
 
+### with all of the indexes in the array alert date do, sub in the index and change the values of the severity to include a colour
 for x in "${!alert_date[@]}"; do
 if [ ${alert_severity[$x]} = "CRITICAL" ];then
     alert_severity[$x]=$(echo -e "\033[31mCRITICAL\e[0m")
@@ -25,6 +28,8 @@ elif [ ${alert_severity[$x]} = "LOW" ];then
     alert_severity[$x]=$(echo -e "\033[32mLOW\e[0m")
 fi
 alert_summary[$x]=$(echo -e "\033[34m${alert_summary[$x]}\e[0m")
+
+### Print all of the above fields into a nicer format
 echo -e "
 ___________________________________________________________________\n       
 ${alert_date[$x]} | ${alert_severity[$x]} | ${alert_elaboration[$x]} 
